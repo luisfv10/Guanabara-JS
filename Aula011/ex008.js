20bedacf906c36a1c4472f12bd065cba6684b608
luisfv10 <lfvp@cin.ufpe.br>
2023-06-13 20:10:00 -0300

    Finishing Class #11

---
+++ Aula011/ex008.js
@@ -0,0 +1,8 @@
+var velocidade = 32.7
+console.log(`A velocidade do seu carro é de ${velocidade}km/hr`)
+console.log(`Dirija usando cinto de segurança`)
+if (velocidade > 60) {
+  console.log("VAI SER MULTADO")
+} else {
+  console.log("Você está dentro do limite de velocidade PARABENS!")
+}
---
+++ Aula011/ex009.js
@@ -0,0 +1,7 @@
+var pais = "Brasil"
+console.log(`Vivendo em ${pais}`)
+if (pais == "Brasil") {
+  console.log("VOCE É BRASILEIRO IRMAO TMJ")
+} else {
+  console.log("Você é estrangeiro")
+}
